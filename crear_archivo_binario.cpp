e5af76cc8e5bbac0a72fe4005363f723ca3c096c
JoseBaquiax123 <79065689+JoseBaquiax123@users.noreply.github.com>
2021-02-28 18:54:20 -0600

    Add files via upload

---
+++ crear_archivo_binario.cpp
@@ -0,0 +1,250 @@
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+
+#include <iostream>
+#include <stdio.h>  
+#include <string.h> 
+
+#define MAX 80
+
+
+
+using namespace std;
+const char *nombe_archivo = "archivo.dat";
+
+struct Estudiante{
+	int codigo;
+	char nombre[50];
+	char apellido[50];
+	int telefono;
+};
+ void ingresar_estudiante();
+void abrir_estudiante();
+ void modificar_estudiante();
+ void eliminar_estudiante();
+ //void eliminar_estudiante();
+ char existeEstudiante(int codigoEstudiante, Estudiante *estudiante);
+ char eliminarEstudiante(int codigoEstudiante);
+ char linea[MAX];
+ int leecad(char *cad, int n);
+ 
+int main (){
+	abrir_estudiante();
+ ingresar_estudiante();
+	modificar_estudiante();
+//	eliminar_estudiante();
+	return 0;	
+}
+void buscar_codigo(){
+	
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	
+	int indice=0,pos=0,cod=0;
+	cout<<"BUSCAR CODIGO: ";
+	cin>>cod;
+	Estudiante estudiante;
+	
+	fread ( &estudiante, sizeof(Estudiante), 1, archivo );
+		
+		do{
+			
+			if (estudiante.codigo == cod){
+			pos = indice;
+			}
+	
+		fread ( &estudiante, sizeof(Estudiante), 1, archivo );
+		indice += 1;
+		} while (feof( archivo ) == 0);
+		
+	cout<<"------------------ "<<pos<<" ------------------"<<endl;
+	fseek ( archivo,pos * sizeof(Estudiante), SEEK_SET );
+
+    fread ( &estudiante, sizeof( Estudiante ), 1, archivo );
+
+    	cout << "Codigo: " << estudiante.codigo << endl;
+        cout << "Nombre: " << estudiante.nombre << endl;
+        cout << "Apellido: " << estudiante.apellido << endl;
+        cout << "Telefono: " << estudiante.telefono << endl;
+        cout << endl;
+	
+	fclose(archivo);
+	system("PAUSE");
+}
+
+
+void buscar_indice(){
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	
+	int pos=0;
+	cout<<"Que registro desea ver: ";
+	cin>>pos;
+	cout<<"------------------ "<<pos<<" ------------------"<<endl;
+	fseek ( archivo,pos * sizeof(Estudiante), SEEK_SET );
+	
+	Estudiante estudiante;
+	
+    fread ( &estudiante, sizeof( Estudiante ), 1, archivo );
+
+    	cout << "Codigo: " << estudiante.codigo << endl;
+        cout << "Nombre: " << estudiante.nombre << endl;
+        cout << "Apellido: " << estudiante.apellido << endl;
+        cout << "Telefono: " << estudiante.telefono << endl;
+        cout << endl;
+	
+	fclose(archivo);
+
+	
+}
+
+
+void abrir_estudiante(){
+
+	system("cls");
+	FILE* archivo = fopen(nombe_archivo, "rb");
+	if(!archivo) {
+		archivo = fopen(nombe_archivo, "w+b");
+	}
+	Estudiante estudiante;
+	int registro=0;
+	fread ( &estudiante, sizeof(Estudiante), 1, archivo );
+	cout<<"____________________________________________________________________"<<endl;
+	cout << "id" <<"|"<< "CODIGO" <<"|"<< "NOMBRE"<<" "<<"APELLIDO"<<" "<<"TELEFONO"<<" "<<endl;	
+		do{
+		cout<<"____________________________________________________________________"<<endl;
+		cout << registro <<" |  "<< estudiante.codigo <<" | "<< estudiante.nombre<<" "<<estudiante.apellido<<" "<<estudiante.telefono<<endl;
+        
+        
+		fread ( &estudiante, sizeof(Estudiante), 1, archivo );
+		registro += 1;	
+		} while (feof( archivo ) == 0);
+		
+    cout<<endl;
+    
+		fclose(archivo);
+	}
+void ingresar_estudiante(){
+	
+	char continuar;
+	FILE* archivo = fopen(nombe_archivo, "ab"); // ab
+	
+	
+		Estudiante estudiante;
+		 string nombre,apellido;
+		do{
+			fflush(stdin);
+			
+		cout<<"Ingrese el Codigo:";
+		cin>>estudiante.codigo;
+        cin.ignore();
+        
+		cout<<"Ingrese el Nombre:";
+		getline(cin,nombre);
+    	strcpy(estudiante.nombre, nombre.c_str()); 
+			
+		cout<<"Ingrese el Apellido:";
+		getline(cin,apellido);
+		strcpy(estudiante.apellido, apellido.c_str()); 
+		
+		cout<<"Ingrese el Telefono:";
+		cin>>estudiante.telefono;
+		cin.ignore();
+		
+		fwrite( &estudiante, sizeof(Estudiante), 1, archivo );
+		
+		cout<<"Desea Agregar otro Nombre s/n : ";
+		cin>>continuar;
+		} while((continuar=='s') || (continuar=='S') );
+		
+	
+	fclose(archivo);
+	abrir_estudiante();
+	//buscar_indice();	
+	buscar_codigo();
+}
+void modificar_estudiante(){
+	
+	FILE* archivo = fopen(nombe_archivo, "r+b");
+	
+		int id;
+		string nombre,apellido;	
+    	Estudiante estudiante;
+    
+		cout << "Ingrese el ID que desea Modificar: ";
+    		cin >> id;
+    		fseek ( archivo, id * sizeof(Estudiante), SEEK_SET );  // esto es lo que permite modificar en la pocision
+	
+		cout<<"Ingrese el Codigo:";
+		cin>>estudiante.codigo;
+        cin.ignore();
+        
+		cout<<"Ingrese el Nombre:";
+		getline(cin,nombre);
+    	strcpy(estudiante.nombre, nombre.c_str()); 
+			
+		cout<<"Ingrese el Apellido:";
+		getline(cin,apellido);
+		strcpy(estudiante.apellido, apellido.c_str()); 
+		
+		cout<<"Ingrese el Telefono:";
+		cin>>estudiante.telefono;
+		cin.ignore();
+		
+		fwrite( &estudiante, sizeof(Estudiante), 1, archivo );
+		
+	fclose(archivo);
+	abrir_estudiante();
+		system("PAUSE");
+}
+void eliminar_estudiante()
+{
+	Estudiante estudiante;
+	int codigo;
+	char repite = 1;
+	char respuesta[MAX];
+ 
+	do {
+		system(CLEAR);
+		printf("\n\t\t\t==> ELIMINAR POR CÓDIGO <==\n");
+ 
+		
+		printf("\n\tId de Estudiante: ");
+		leecad(linea, MAX);
+		sscanf(linea, "%d", &codigo);
+ 
+		
+		if (existeEstudiante(codigo, &estudiante)) {   
+ 
+			
+			printf("\n\tCódigo del estudiante: %d\n", estudiante.codigo);
+			printf("\tNombre: %s\n", estudiante.nombre);			
+			printf("\tApellidos: %.1f $\n", estudiante.apellido);
+			printf("\tTelefono: %.1f $\n", estudiante.telefono);
+ 
+			printf("\n\tSeguro que desea eliminar el Estudiante? [S/N]: ");
+			leecad(respuesta, MAX);
+			if (strcmp(respuesta, "S") == 0 || strcmp(respuesta, "s") == 0) {
+				if (eliminarEstudiante(codigo)) {
+					printf("\n\tProducto eliminado satisfactoriamente.\n");
+				} else {
+					printf("\n\tEl producto no pudo ser eliminado\n");
+				}
+			}
+ 
+		} else {
+			
+			printf("\n\tEl producto de código %d no existe.\n", codigo);
+		}
+ 
+		printf("\n\tDesea eliminar otro producto? [S/N]: ");
+		leecad(respuesta, MAX);
+ 
+		if (!(strcmp(respuesta, "S") == 0 || strcmp(respuesta, "s") == 0)) {
+			repite = 0;
+		}
+ 
+	} while (repite);
+}
+
